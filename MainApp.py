9c51d044bad11bb781294b1a8ca0020282d86c01
Martin Landa <landa.martin@gmail.com>
2018-06-01 12:51:37 +0200

    switch to GDAL-VFR 2.0.5

--- MainApp.py
+++ MainApp.py
@@ -44,7 +44,7 @@ except:
     # download
     import requests, zipfile, StringIO
 
-    gdal_vfr_version = '2.0.4'
+    gdal_vfr_version = '2.0.5'
     url = "http://geo.fsv.cvut.cz/geoforall/gdal_vfr/v{}.zip".format(gdal_vfr_version)
     req = requests.get(url, stream=True)
     zipf = zipfile.ZipFile(StringIO.StringIO(req.content))
