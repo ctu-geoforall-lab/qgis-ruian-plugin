5b05d50e426c79772e1806a151d70429adf4bc6d
Martin Landa <landa.martin@gmail.com>
2019-02-18 22:16:52 +0100

    publish script tuning

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 TMP=/tmp
 DIR=$TMP/qgis-ruian-plugin
@@ -15,7 +15,7 @@ rm -rf $DIR/gdal_vfr
 # get version
 gdal_vfr_version=`grep "gdal_vfr_version = " MainApp.py | cut -d'=' -f2 | sed -E 's/^[ \t]'\''(.*)'\''/\1/g'`
 cd $DIR
-wget https://github.com/ctu-geoforall-lab/gdal-vfr/archive/v$gdal_vfr_version.zip
+wget -q https://github.com/ctu-geoforall-lab/gdal-vfr/archive/v$gdal_vfr_version.zip
 unzip v$gdal_vfr_version.zip
 mv gdal-vfr-$gdal_vfr_version gdal_vfr
 rm v$gdal_vfr_version.zip
@@ -25,9 +25,11 @@ rm -rf .git *~ publish.sh __pycache__
 find . -name "*.pyc" -exec rm -f {} \;
 
 # packaging
+version=`grep 'version=' metadata.txt | cut -d'=' -f2`
 cd ..
-rm qgis-ruian-plugin.zip
-zip qgis-ruian-plugin.zip qgis-ruian-plugin -r
-
+FILE=qgis-ruian-plugin-final-$version.zip
+rm -rf $FILE
+zip $FILE qgis-ruian-plugin -rq
+echo "$FILE"
 
 exit 0
