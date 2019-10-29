d76f76b5c322c600a75fc062541ae20e0b7ce213
Martin Landa <landa.martin@gmail.com>
2019-10-29 13:23:49 +0100

    add helper create_package script

---
+++ scripts/create_package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+GDAL_VFR_VERSION=2.1.4
+
+rm -rf gdal_vfr
+wget https://github.com/ctu-geoforall-lab/gdal-vfr/archive/v${GDAL_VFR_VERSION}.tar.gz
+tar xvzf v${GDAL_VFR_VERSION}.tar.gz
+mv gdal-vfr-${GDAL_VFR_VERSION} gdal_vfr
+rm v${GDAL_VFR_VERSION}.tar.gz
+echo 'Y' | pb_tool zip
+
+exit 0
