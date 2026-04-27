ac66580ce1660df4a7e5b0b2ddb1e94f1fdc1a8d
Martin Landa <landa.martin@gmail.com>
2026-04-27 13:26:14 +0200

    fix  gdal-vfr

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -6,7 +6,7 @@ rm -rf gdal_vfr
 wget https://github.com/ctu-geoforall-lab/gdal-vfr/archive/v${GDAL_VFR_VERSION}.tar.gz
 tar xvzf v${GDAL_VFR_VERSION}.tar.gz
 mkdir gdal_vfr
-mv gdal-vfr-${GDAL_VFR_VERSION}/vfr4ogr gdal_vfr/
+mv gdal-vfr-${GDAL_VFR_VERSION}/vfr4ogr gdal-vfr-${GDAL_VFR_VERSION}/vfr4ogr.conf gdal_vfr/
 rm v${GDAL_VFR_VERSION}.tar.gz gdal-vfr-${GDAL_VFR_VERSION}
 echo 'Y' | pb_tool zip
 
