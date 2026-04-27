f6787eaf810b6d6a2294586f5779c46c30e54808
Martin Landa <landa.martin@gmail.com>
2026-04-27 12:53:16 +0200

    remove gdal_vfr binaries - avoid bandit's warning

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -6,6 +6,7 @@ rm -rf gdal_vfr
 wget https://github.com/ctu-geoforall-lab/gdal-vfr/archive/v${GDAL_VFR_VERSION}.tar.gz
 tar xvzf v${GDAL_VFR_VERSION}.tar.gz
 mv gdal-vfr-${GDAL_VFR_VERSION} gdal_vfr
+rm gdal_vfr/*.bat gdal_vfr/test_suite/*.sh gdal_vfr/test_suite/*.bat
 rm v${GDAL_VFR_VERSION}.tar.gz
 echo 'Y' | pb_tool zip
 
